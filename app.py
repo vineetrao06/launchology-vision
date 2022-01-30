b669f389de2e2c1f6366aeac208108c1b2f6e158
amaroo2006 <amaroo2006@gmail.com>
2022-01-29 23:11:08 -0800

    fixed file placement

---
+++ app.py
@@ -0,0 +1,36 @@
+import os
+import csv
+import random as rand
+
+from flask import Flask, jsonify
+from flask import request
+
+app = Flask(__name__)
+
+
+inputXY = []
+prev = []
+
+
+@app.route("/main", methods=["POST"])
+def data():
+    if request.method == "POST":
+        # get data from post request
+        data = request.form.to_dict(flat=False)
+
+        # parse data into integers
+        x = int(data["data1"][0])
+        y = int(data["data2"][0])
+
+        # append to csv
+        with open("feelings.csv", "w") as file:
+            writer = csv.writer(file)
+            writer.writerow((x, y))
+
+    return "success"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+# adds new data
